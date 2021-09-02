dcc0395e1acc9e460b9071c132ddbbae31a028dc
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2021-09-01 21:33:29 -0300

    creating file trapezio.c

---
+++ trapezio.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<mpi.h>
+
+double trapezio(double limite_esq, double limite_dir, int numero_trap, double tam_altura);
+
+double f(double x);
+
+int main(void){
+    int meu_rank, comm_sz, n = 1024, local_n;
+    double a = -2.0, b = 2.0, h, local_a, local_b;
+    double local_int, total_int;
+    int origem;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &meu_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    
+    h = (b - a)/n;
+    local_n = n/comm_sz;
+    local_a = a + meu_rank*local_n*h;
+    local_b = local_a + local_n*h;
+    local_int = trapezio(local_a, local_b, local_n, h);
+
+    if (meu_rank != 0){
+        MPI_Send(&local_int, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }else{
+        total_int = local_int;
+        for (origem = 1; origem < comm_sz; origem++){
+            MPI_Recv(&local_int, 1, MPI_DOUBLE, origem, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            total += local_int;
+        }
+    }
+
+    if (meu_rank == 0){
+        printf("Com n = %d trapezios, nossa estimativa\n", n);
+        printf("da integral de %f ate %f = %.15e\n", a, b, total_int);
+    }
+
+
+}
